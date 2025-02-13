8cad51dcc00ca6d7a76a1da70222759f0db182e2
Rusyaha <barcer@yandex.ru>
2025-02-13 13:43:52 +0300

    Дополнил конфиги

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,65 @@ const gulp = require('gulp');
 const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
-const browserSync = require('browser-sync').create(); 
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+} 
 
 function html() {
   return gulp.src('src/**/*.html')
         .pipe(plumber())
                 .pipe(gulp.dest('dist/'))
+                .pipe(browserSync.reload({stream: true}));
 }
 
-exports.html = html; 
-
 function css() {
+  const plugins = [];
   return gulp.src('src/**/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
+        .pipe(postcss(plugins))
                 .pipe(gulp.dest('dist/'))
+                .pipe(browserSync.reload({stream: true}));
 }
-
-exports.css = css; 
 
 function images() {
   return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
             .pipe(gulp.dest('dist/images'))
+            .pipe(browserSync.reload({stream: true}));
 }
-
-exports.images = images; 
 
 function fonts() {
   return gulp.src('src/fonts/**/*.woff')
             .pipe(gulp.dest('dist/fonts'))
+            .pipe(browserSync.reload({stream: true}));
 }
 
-exports.fonts = fonts; 
+function clean() {
+  return del('dist');
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts)); 
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/styles/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.woff'], fonts);
+}
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
